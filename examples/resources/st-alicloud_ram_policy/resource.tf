ee0eaeaed4d9ef432ec645d5cb48fe64e210e245
LiangQuanWong <liangquanwong@ipa.sige.la>
2025-01-06 12:32:36 +0800

    Reset resource.tf to original file

--- examples/resources/st-alicloud_ram_policy/resource.tf
+++ examples/resources/st-alicloud_ram_policy/resource.tf
@@ -1,16 +1,4 @@
 resource "st-alicloud_ram_policy" "ram_policy" {
-  attached_policies = ["AliyunRAMReadOnlyAccess","LqTestPolicy"]
-  user_name         = "lq-user-2"
-}
-
-terraform {
-  required_providers {
-    st-alicloud = {
-      source = "example.local/myklst/st-alicloud"
-    }
-  }
-}
-
-provider "st-alicloud" {
-  region = "cn-hongkong"
+  attached_policies = ["AliyunECSFullAccess", "AliyunRAMFullAccess", "AliyunOSSFullAccess", "AliyunOTSFullAccess", ]
+  user_name         = "devopsuser01"
 }
